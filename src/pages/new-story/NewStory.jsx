2a45732e12f318d039739c5f253a0cf05d0baeee
massiliatazit <massiliatazit@users.noreply.github.com>
2021-01-12 23:32:24 +0100

    post new story and get articles to read

--- src/pages/new-story/NewStory.jsx
+++ src/pages/new-story/NewStory.jsx
@@ -9,11 +9,14 @@ import CategoryPicker from "../../components/CategoryPicker";
 export default class NewStory extends Component {
   state = {
     html: "",
+    category: {},
+    title: "",
+    img: "",
   };
   editor = React.createRef();
   onChange = (html) => {
-    this.setState({ html })
-    console.log(html)
+    this.setState({ html });
+    console.log(html);
   };
   onKeyDown = (e) => {
     if (e.key === "Enter") {
@@ -21,17 +24,55 @@ export default class NewStory extends Component {
       this.editor && this.editor.current.focus();
     }
   };
+  submitButton = (e) => {
+    e.preventDefault();
+    this.postArticle();
+  };
+  postArticle = async () => {
+    try {
+      let body = {
+        category: this.state.category,
+        author: {
+          name: "Massyli",
+          img:
+            "https://media.discordapp.net/attachments/781180012526436352/790971313836916756/unknown.png",
+        },
+        headLine: this.state.title,
+        subHead: "kikouuu",
+        content: this.state.html,
+        cover: this.state.img,
+      };
+      console.log(body);
+      let response = await fetch("http://localhost:4001/articles/", {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      this.props.history.push("/");
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   render() {
     const { html } = this.state;
     return (
       <Container className="new-story-container" expand="md">
         <div className="category-container">
-        <CategoryPicker onChange={(topic)=>{console.log(topic)}} />
+          <CategoryPicker
+            onChange={(topic) => {
+              this.setState({ category: { name: topic.name, img: topic.img } });
+            }}
+          />
         </div>
         <input
           onKeyDown={this.onKeyDown}
           placeholder="Title"
           className="article-title-input"
+          value={this.state.title}
+          onChange={(e) => this.setState({ title: e.currentTarget.value })}
         />
 
         <ReactQuill
@@ -47,9 +88,15 @@ export default class NewStory extends Component {
           onKeyDown={this.onKeyDown}
           placeholder="Cover link e.g : https://picsum.photos/800"
           className="article-cover-input"
+          value={this.state.img}
+          onChange={(e) => this.setState({ img: e.currentTarget.value })}
         />
-       
-        <Button variant="success" className="post-btn">
+
+        <Button
+          variant="success"
+          className="post-btn"
+          onClick={(e) => this.submitButton(e)}
+        >
           Post
         </Button>
       </Container>
--- src/pages/read/Read.jsx
+++ src/pages/read/Read.jsx
@@ -2,51 +2,71 @@ import React, { Component } from "react";
 import { Container, Row, Col, Image } from "react-bootstrap";
 import { withRouter } from "react-router-dom";
 import "./index.scss";
-import article from "./data.json";
+// import article from "./data.json";
 import { IoLogoTwitter, IoLogoLinkedin, IoLogoFacebook } from "react-icons/io";
 import { IoBookmarkOutline } from "react-icons/io5";
-import Reactions from "../../components/Reactions/Reactions"
+import Reactions from "../../components/Reactions/Reactions";
 class Read extends Component {
+  state = {
+    article: {},
+  };
+  componentDidMount = () => {
+    this.fetchArticle();
+  };
+
+  fetchArticle = async () => {
+    let id = this.props.match.params.slug;
+    try {
+      let response = await fetch(`http://localhost:4001/articles/${id}`);
+      let article = await response.json();
+      console.log(article);
+      this.setState({ article: article });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  htmlArticle = () => {
+    return { __html: this.state.article.content };
+  };
+
   render() {
     return (
-      <Container className="article-container">
-        <h1>{article.title}</h1>
-        <Row style={{ marginTop: 20, marginBottom: 20 }}>
-          <Col xs={1}>
-            <Image
-              style={{ width: 50, height: 50 }}
-              src="https://miro.medium.com/fit/c/96/96/1*xVwJ4C9D1sjrRc-sR_jO0w.jpeg"
-              roundedCircle
-            />
-          </Col>
-          <Col>
-            {article.author.name}
-            <p>Sep 23, 2018 · 3 min read</p>
-          </Col>
-          <Col>
-            <div
-              style={{
-                fontSize: 24,
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "flex-end",
-              }}
-            >
-              <IoLogoTwitter />
-              <IoLogoLinkedin />
-              <IoLogoFacebook />
-              <IoBookmarkOutline />
-            </div>
-          </Col>
-        </Row>
-        <p>{article.content}</p>
-        <p>{article.content}</p>
-        <p>{article.content}</p>
-        <p>{article.content}</p>
-        <p>{article.content}</p>
-        <p>{article.content}</p>
-        <Reactions/>
-      </Container>
+      this.state.article.hasOwnProperty("createdAt") && (
+        <Container className="article-container">
+          <h1>{this.state.article.headLine}</h1>
+          <Row style={{ marginTop: 20, marginBottom: 20 }}>
+            <Col xs={1}>
+              <Image
+                style={{ width: 50, height: 50 }}
+                src={this.state.article.author.img}
+                roundedCircle
+              />
+            </Col>
+            <Col>
+              {this.state.article.author.name}
+              <p>Sep 23, 2018 · 3 min read</p>
+            </Col>
+            <Col>
+              <div
+                style={{
+                  fontSize: 24,
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "flex-end",
+                }}
+              >
+                <IoLogoTwitter />
+                <IoLogoLinkedin />
+                <IoLogoFacebook />
+                <IoBookmarkOutline />
+              </div>
+            </Col>
+          </Row>
+          <div dangerouslySetInnerHTML={this.htmlArticle()} />
+          <Reactions />
+        </Container>
+      )
     );
   }
 }
